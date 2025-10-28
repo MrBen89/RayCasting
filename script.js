ccbbed13fb0734c7e833ff0be8de584c93d7573b
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2025-10-28 12:52:45 +0900

    added vision pointer (movement doesnt align yet)

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let block_array = [[1,1,1,1,1,1,1,1,1,1],
                [1,1,1,1,1,1,1,1,1,1]];
 
 let start_coord = [5,5];
+let player_facing = 0;
+let scale = 40;
 
 let canvas_grid = document.getElementById("grid")
 let canvas_display = document.getElementById("display");
@@ -22,16 +24,25 @@ let clearcanvas = () => {
 }
 
 let draw_player = () => {
-    grid_ctx.fillStyle = 'red'; // Set the fill color
-    grid_ctx.fillRect((start_coord[0]*40 + 15), (start_coord[1]*40 + 15), 10, 10);
+    grid_ctx.fillStyle = 'red'; 
+    grid_ctx.fillRect((start_coord[0]*scale + 15), (start_coord[1]*scale + 15), 10, 10);
 }
 
-draw_player();
-
 let draw_block = (x, y) => {
-    grid_ctx.fillStyle = 'grey'; // Set the fill color
-    grid_ctx.fillRect((x*40)+1, (y*40)+1, 38, 38);
-    console.log(x)
+    grid_ctx.fillStyle = 'grey'; 
+    grid_ctx.fillRect((x*scale)+1, (y*scale)+1, 38, 38);
+}
+
+let draw_facing = () => {
+    const startx = start_coord[0]*scale + 20
+    const starty = start_coord[1]*scale + 20
+    const endx = startx +100 * Math.cos(player_facing)
+    const endy = starty +100 * Math.sin(player_facing);
+    grid_ctx.beginPath();
+    grid_ctx.moveTo(startx, starty);
+    grid_ctx.lineTo(endx, endy);
+    grid_ctx.stroke();
+
 }
 
 let draw_layout = () => {
@@ -40,43 +51,46 @@ let draw_layout = () => {
             if (block_array[y][x] == 1){
                 draw_block(x,y);
             }
-            
-            // console.log(x)
         }
     }
 }
 
+draw_player();
 draw_layout();
 
 document.addEventListener("keydown", (event) => {
     switch (event.key) {
         case "ArrowUp":
             start_coord[1] > 1 ? start_coord[1] -= 1 : start_coord[1] = 1;
-            console.log(start_coord)
             clearcanvas();
             draw_player();
             draw_layout();
+            draw_facing();
             break;
         case "ArrowDown":
             start_coord[1] < 8 ? start_coord[1] += 1 : start_coord[1] = 8;
-            console.log(start_coord)
             clearcanvas();
             draw_player();
             draw_layout();
+            draw_facing();
             break;
         case "ArrowLeft":
             start_coord[0] > 1 ? start_coord[0] -= 1 : start_coord[0] = 1;
-            console.log(start_coord)
+            player_facing == 1.5 * Math.PI ? player_facing = 0 : player_facing += Math.PI/2;
             clearcanvas();
             draw_player();
             draw_layout();
+            draw_facing();
+            console.log(player_facing)
             break;
         case "ArrowRight":
             start_coord[0] < 8 ? start_coord[0] += 1 : start_coord[0] = 8;
-            console.log(start_coord)
+            player_facing == 0 ? player_facing = 1.5 * Math.PI : player_facing -= Math.PI/2;
             clearcanvas();
             draw_player();
             draw_layout();
+            draw_facing();
+            console.log(player_facing)
             break;
     }
  })
