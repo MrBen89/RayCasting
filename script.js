ac83313ac711be6222d6ea7e1fea14e1e0f3bbbd
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2025-10-28 20:31:24 +0900

    switch to tank controls

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ let draw_block = (x, y) => {
 let draw_facing = () => {
     const startx = start_coord[0]*scale + 20
     const starty = start_coord[1]*scale + 20
-    const endx = startx +100 * Math.cos(player_facing)
-    const endy = starty +100 * Math.sin(player_facing);
+    const endx = startx +20 * Math.cos(player_facing)
+    const endy = starty +20 * Math.sin(player_facing);
     grid_ctx.beginPath();
     grid_ctx.moveTo(startx, starty);
     grid_ctx.lineTo(endx, endy);
@@ -56,27 +56,53 @@ let draw_layout = () => {
 }
 
 draw_player();
+draw_facing();
 draw_layout();
 
 document.addEventListener("keydown", (event) => {
     switch (event.key) {
         case "ArrowUp":
-            start_coord[1] > 1 ? start_coord[1] -= 1 : start_coord[1] = 1;
+            switch (player_facing) {
+                case 0:
+                    start_coord[0] < 8 ? start_coord[0] += 1 : start_coord[0] = 8;
+                    break;
+                case Math.PI*1.5:
+                    start_coord[1] > 1 ? start_coord[1] -= 1 : start_coord[1] = 1;
+                    break;
+                case Math.PI:
+                    start_coord[0] > 1 ? start_coord[0] -= 1 : start_coord[0] = 1;
+                    break;
+                case Math.PI/2:
+                    start_coord[1] < 8 ? start_coord[1] += 1 : start_coord[1] = 8;
+                    break;
+            }
             clearcanvas();
             draw_player();
             draw_layout();
             draw_facing();
             break;
         case "ArrowDown":
-            start_coord[1] < 8 ? start_coord[1] += 1 : start_coord[1] = 8;
+             switch (player_facing) {
+                case 0:
+                    start_coord[0] > 1 ? start_coord[0] -= 1 : start_coord[0] = 1;   
+                    break;
+                case Math.PI*1.5:
+                    start_coord[1] < 8 ? start_coord[1] += 1 : start_coord[1] = 8;
+                    break;
+                case Math.PI:
+                    start_coord[0] < 8 ? start_coord[0] += 1 : start_coord[0] = 8;
+                    break;
+                case Math.PI/2:
+                    start_coord[1] > 1 ? start_coord[1] -= 1 : start_coord[1] = 1;
+                    break;
+            }
             clearcanvas();
             draw_player();
             draw_layout();
             draw_facing();
             break;
         case "ArrowLeft":
-            start_coord[0] > 1 ? start_coord[0] -= 1 : start_coord[0] = 1;
-            player_facing == 1.5 * Math.PI ? player_facing = 0 : player_facing += Math.PI/2;
+            player_facing == 0 ? player_facing = 1.5 * Math.PI : player_facing -= Math.PI/2;
             clearcanvas();
             draw_player();
             draw_layout();
@@ -84,8 +110,7 @@ document.addEventListener("keydown", (event) => {
             console.log(player_facing)
             break;
         case "ArrowRight":
-            start_coord[0] < 8 ? start_coord[0] += 1 : start_coord[0] = 8;
-            player_facing == 0 ? player_facing = 1.5 * Math.PI : player_facing -= Math.PI/2;
+            player_facing == 1.5 * Math.PI ? player_facing = 0 : player_facing += Math.PI/2;
             clearcanvas();
             draw_player();
             draw_layout();
